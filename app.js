39f924802f06e21d174f253aa41e01ecac09a235
shrifzain <162094463+shrifzain@users.noreply.github.com>
2024-11-17 14:40:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!yyyyyyyyyyyy');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
