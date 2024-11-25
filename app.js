d6abfe2581e0fd63638fb6b983cdce3f9a95cb49
shrifzain <162094463+shrifzain@users.noreply.github.com>
2024-11-25 19:48:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 // Define a basic route
 app.get('/', (req, res) => {
-  res.send('Hello, helleeeervrgrvgbttfvffccggrero');
+  res.send('Hello, helleeeervrgrffccggrero');
 });
 
 // Start the server
